06abcb6fea6f6cf73e4498e72ab589134f5c9d14
janakkumar2003 <72163118+janakkumar2003@users.noreply.github.com>
2020-10-01 00:53:04 +0530

    commented it

--- 2nd 1Q.c
+++ 2nd 1Q.c
@@ -1,9 +1,9 @@
-#include<stdio.h.>
-int main()
+#include<stdio.h.>              //including standard io library
+int main()                      //initializing main function
 {
-    int a,b,c,d,e,f,g;
+    int a,b,c,d,e,f,g;              
     printf("enter 5 digit no");
-    scanf("%d",&a);
+    scanf("%d",&a);                   //reading 5 digit number
 
     a=(b*10000)+(c*1000)+(d*100)+(e*10)+f;
     scanf("%d,%d,%d,%d,%d",&b,&c,&d,&e,&f);
